e921196c9de2798b30ad6d3fc1f8fb5ad23a324c
jjowoo <1201wogus@naver.com>
2022-10-01 12:34:06 +0900

    10/1

--- app/src/main/java/com/example/javaappembedded/SignIn.java
+++ app/src/main/java/com/example/javaappembedded/SignIn.java
@@ -3,15 +3,76 @@ package com.example.javaappembedded;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
-    EditText phone_num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mAuth = FirebaseAuth.getInstance(); //
+        findViewById(R.id.btn_signin).setOnClickListener(onClickListener);
+    }
 
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_signin:
+                    signUp();
+                    break;
+            }
+        }
+    };
+
+
+    private void signUp(){
+        String name=((EditText)findViewById(R.id.editText_name)).getText().toString();
+        String email=((EditText)findViewById(R.id.editText_email)).getText().toString();
+        String password=((EditText)findViewById(R.id.editText_password)).getText().toString();
+        String password_confirm=((EditText)findViewById(R.id.editText_password_confirm)).getText().toString();
+
+        if(email.length()>0 && password.length()>0 && password_confirm.length()>0){
+            if(password.equals(password_confirm)){
+                //firestore에 email,password 저장
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("TAG", "createUserWithEmail:success");
+                                    Toast.makeText(SignIn.this, "회원가입에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("TAG", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(SignIn.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+            else{
+                Toast.makeText(SignIn.this, "비밀번호가 일치하지 않습니다." ,Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(SignIn.this, "아아디와 비밀번호를 확인해주세요." ,Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/javaappembedded/login.java
+++ app/src/main/java/com/example/javaappembedded/login.java
@@ -1,12 +1,22 @@
 package com.example.javaappembedded;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -14,28 +24,91 @@ public class login extends AppCompatActivity {
 
     EditText editText_email,editText_Password;
     Button button_login;
+    TextView signup;
+    FirebaseAuth fAuth;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        editText_email = findViewById(R.id.editText_email);
+        editText_Password = findViewById(R.id.editText_Password);
+        button_login = findViewById(R.id.button_login);
+        signup = findViewById(R.id.signup);
+        fAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        editText_email=findViewById(R.id.editText_email);
-        editText_Password=findViewById(R.id.editText_Password);
-        button_login=findViewById(R.id.button_login);
+        //signup 누르면 signInActivity로 이동
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SignIn.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
 
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email=editText_email.getText().toString();
-                String password= editText_Password.getText().toString();
-
-                Intent intent=new Intent(login.this, Mainpage.class);
-                intent.putExtra("email",email);
-                intent.putExtra("password",password);
-                startActivity(intent);
+                if (!editText_email.getText().toString().equals("") && !editText_Password.getText().toString().equals("")) {
+                    loginUser(editText_email.getText().toString(), editText_Password.getText().toString());
+                } else {
+                    Toast.makeText(login.this, "계정과 비밀번호를 입력하세요.", Toast.LENGTH_LONG).show();
+                }
             }
         });
+
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(login.this, Mainpage.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                }
+            }
+        };
+    }
+
+    public void loginUser(String email, String password) {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // 로그인 성공
+                            Toast.makeText(com.example.javaappembedded.login.this, "로그인 성공", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.addAuthStateListener(firebaseAuthListener);
+                        } else {
+                            // 로그인 실패
+                            Toast.makeText(com.example.javaappembedded.login.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
     }
 }
